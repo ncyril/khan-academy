c48b93b86f4f38d5407e9fbab7da1ae9f27dc056
ncyril <nigeljcyril@gmail.com>
2023-12-05 01:01:48 -0500

    Added Basics of Javascript and updated my_portfolio with Bootstrap JS

---
+++ 5-JavaScript/1-variables.js
@@ -0,0 +1,27 @@
+//This is a singular line of code commenting
+
+/*
+This is how we comment 
+Multi line of codes
+*/
+
+// Statements: JavaScript code is composed of statements that perform specific actions. Each statement typically ends with a semicolon (;), although it is not always required.
+
+// Comments: You can add comments in JavaScript to provide explanations or make notes within the code. There are two types of comments: single-line comments, starting with //, and multi-line comments, enclosed between /* and */.
+
+// Case sensitivity: JavaScript is case-sensitive, meaning that uppercase and lowercase letters are treated differently. For example, "variable" and "Variable" would be considered as two separate variables.
+
+// Whitespace: JavaScript ignores spaces, tabs, and line breaks (collectively called whitespace) that are not necessary for code execution. However, whitespace can enhance code readability.
+
+// Blocks: Code blocks in JavaScript are enclosed in curly braces ({}) and are used to group statements together. Blocks are often used in control structures like loops and conditional statements.
+
+//Variables
+let name = "John";
+console.log("This is the name of the user:", name);
+
+
+let myFavoriteGames = "Baldurs Gate 3";
+console.log("My favorite game is:", myFavoriteGames);
+
+const gravity = "9.8"
+const birthday = "January 1st"
